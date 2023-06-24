000051d3bd482ac363b0b6f70b5cdb9d1363fe63
valerii.korniienko <vakor.dev@gmail.com>
2023-06-24 23:51:47 +0300

    Primary fixes

--- Assets/Scripts/WeaponSystem/RayGun.cs
+++ Assets/Scripts/WeaponSystem/RayGun.cs
@@ -1,59 +1,59 @@
 #region
 
-using Core;
 using UnityEngine;
-using UnityEngine.Serialization;
-using WeaponSystem;
 using Random = UnityEngine.Random;
 
 #endregion
 
-public class RayGun : MonoBehaviour
+namespace WeaponSystem
 {
-    [SerializeField] private Transform shootingPoint;
-    [SerializeField] private Transform barrelPoint;
-    [SerializeField] private LayerMask shootingMask;
-    [SerializeField] private GameObject bullet; 
-    
-    [SerializeField] private LineRenderer rayVisualizer;
-
-    private float _shootingTimer;
-    [SerializeField]private Weapon weapon;
-    
-    private void Start()
+    public class RayGun : MonoBehaviour
     {
-        rayVisualizer.enabled = false;
-    }
-
-    public void Shoot()
+        [SerializeField] private Transform shootingPoint;
+        [SerializeField] private Transform barrelPoint;
+        [SerializeField] private LayerMask shootingMask;
+        [SerializeField] private GameObject bullet; 
     
-    {
-        Vector3 spread = Random.insideUnitSphere * Random.Range(0, weapon.Spread);
-        Vector3 direction = (shootingPoint.forward + spread).normalized;
+        [SerializeField] private LineRenderer rayVisualizer;
 
-        var currentBullet = Instantiate(bullet, shootingPoint.position, Quaternion.identity);
-        currentBullet.GetComponent<BulletController>().Init(weapon);
-        currentBullet.transform.forward = direction;
-        currentBullet.GetComponent<Rigidbody>().AddForce(direction*weapon.ShootForce, ForceMode.Impulse);
+        private float _shootingTimer;
+        [SerializeField]private Weapon weapon;
+    
+        private void Start()
+        {
+            rayVisualizer.enabled = false;
+        }
+
+        public void Shoot()
+    
+        {
+            Vector3 spread = Random.insideUnitSphere * Random.Range(0, weapon.Spread);
+            Vector3 direction = (shootingPoint.forward + spread).normalized;
+
+            var currentBullet = Instantiate(bullet, shootingPoint.position, Quaternion.identity);
+            currentBullet.GetComponent<BulletController>().Init(weapon);
+            currentBullet.transform.forward = direction;
+            currentBullet.GetComponent<Rigidbody>().AddForce(direction*weapon.ShootForce, ForceMode.Impulse);
         
-        var ray = new Ray(shootingPoint.position, direction);
-        bool hasHit = Physics.Raycast(ray, out var hit, weapon.MaxDistance, shootingMask);
-        ShotDraw(ray, hasHit, hit, barrelPoint.position);
-    }
+            var ray = new Ray(shootingPoint.position, direction);
+            bool hasHit = Physics.Raycast(ray, out var hit, weapon.MaxDistance, shootingMask);
+            ShotDraw(ray, hasHit, hit, barrelPoint.position);
+        }
 
-    private void ShotDraw(Ray ray, bool hasHit, RaycastHit hitInfo, Vector3 barrelPointPosition)
-    {
-        rayVisualizer.enabled = true;
-        rayVisualizer.SetPosition(0, barrelPointPosition);
-        if(hasHit)
-            rayVisualizer.SetPosition(1, hitInfo.point);
-        else
-            rayVisualizer.SetPosition(1, ray.origin + ray.direction * weapon.MaxDistance);
-    }
+        private void ShotDraw(Ray ray, bool hasHit, RaycastHit hitInfo, Vector3 barrelPointPosition)
+        {
+            rayVisualizer.enabled = true;
+            rayVisualizer.SetPosition(0, barrelPointPosition);
+            if(hasHit)
+                rayVisualizer.SetPosition(1, hitInfo.point);
+            else
+                rayVisualizer.SetPosition(1, ray.origin + ray.direction * weapon.MaxDistance);
+        }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(shootingPoint.position, shootingPoint.forward * weapon.MaxDistance);
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(shootingPoint.position, shootingPoint.forward * weapon.MaxDistance);
+        }
     }
 }
