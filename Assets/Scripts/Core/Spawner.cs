d8beab393781770f27b5cd449430403865249b26
valerii.korniienko <vakor.dev@gmail.com>
2023-06-24 18:57:24 +0300

    Added enemies variation

---
+++ Assets/Scripts/Core/Spawner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Core
+{
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField] private EnemyAndProbability[] spawnVariants;
+
+        private void Start()
+        {
+            SetValuesNormalized();
+        }
+
+        private Enemy GetNextEnemy()
+        {
+            float randomValue = Random.value;
+            float currentValue = randomValue;
+            for (int i = 0; i < spawnVariants.Length; i++)
+            {
+                float spawnProbability = spawnVariants[i].ValueNormalized;
+                if (currentValue < spawnProbability)
+                {
+                    return spawnVariants[i].enemyPrefab;
+                }
+
+                currentValue -= spawnProbability;
+            }
+
+            return spawnVariants[^1].enemyPrefab;
+        }
+
+        private void SetValuesNormalized()
+        {
+            float totalValue = 0f;
+            for (var i = 0; i < spawnVariants.Length; i++)
+            {
+                totalValue += spawnVariants[i].value;
+            }
+            
+            for (var i = 0; i < spawnVariants.Length; i++)
+            {
+                spawnVariants[i].ValueNormalized = spawnVariants[i].value / totalValue;
+            }
+        }
+    }
+
+    [Serializable]
+    public struct EnemyAndProbability
+    {
+        public Enemy enemyPrefab;
+        public float value;
+        public float ValueNormalized { get; set; }
+    }
+}
