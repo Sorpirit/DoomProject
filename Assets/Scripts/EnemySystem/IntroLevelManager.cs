0b469bcb6125bca43c433fed35544251761fd2d3
valerii.korniienko <vakor.dev@gmail.com>
2023-06-26 19:06:57 +0300

    IntroScene finished

---
+++ Assets/Scripts/EnemySystem/IntroLevelManager.cs
@@ -0,0 +1,20 @@
+﻿using EnemySystem.AI;
+using ObjectSystem;
+using UnityEngine;
+
+namespace EnemySystem
+{
+    public class IntroLevelManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            HealthPill.OnPillPicked += HealthPillOnPillPicked;
+        }
+
+        private void HealthPillOnPillPicked()
+        {
+            HealthPill.OnPillPicked -= HealthPillOnPillPicked;
+            SceneLoader.LoadScene(SceneLoader.Scene.Level0);
+        }
+    }
+}
